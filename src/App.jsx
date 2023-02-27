c73eb6ab285e6c02515ffb6e669e116efd7c7b95
JayThiraphat <wongwatnicha@gmail.com>
2023-02-28 05:04:14 +0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import DropdownComponent from './components/DropdownComponent'
+import FoodComponent from './components/FoodComponent'
+import MenuData from './data/MenuData'
+function App() {
+
+  const [foods,setFoods] = useState(MenuData)
+  const changeFoodData =(e)=>{
+    const category = e.target.value
+    if(category === "เมนูทั้งหมด"){
+      setFoods(MenuData)
+    }else{
+      const result= MenuData.filter((element)=>{
+      return element.menu === category
+      })
+      setFoods(result)
+    }
+
+  }
+  return (
+    <div className="container">
+      <DropdownComponent changeFoodData={changeFoodData}/>
+      <div className="content">
+        {foods.map((data,index)=>{
+          return <FoodComponent key={index} {...data}/>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
